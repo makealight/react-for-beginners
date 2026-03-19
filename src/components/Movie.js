be430b294a4f788f9f085e7f76bc3bcced27824f
cskman <makealight@gmail.com>
2026-03-19 16:47:53 +0900

    #7.9 Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,28 +1,35 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, title, poster_path, overview, genre_ids }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <img src={`https://wsrv.nl/?url=${poster_path}`} alt={title} />
-      <p>{overview}</p>
-      <ul>
-        {genre_ids.map((g, index) => (
-          <li key={index}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <Link to={`/movie/${id}`}>
+        <img src={coverImg} alt={title} className={styles.movie__img} />
+      </Link>
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 225 ? `${summary.slice(0, 225)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-Movie.prototype = {
+Movie.propTypes = {
   id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  poster_path: PropTypes.string.isRequired,
-  overview: PropTypes.string.isRequired,
-  genre_ids: PropTypes.arrayOf(PropTypes.string).isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
+
 export default Movie;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,38 +1,66 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+const GENRE_MAP = {
+  28: "Action",
+  12: "Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  14: "Fantasy",
+  36: "History",
+  27: "Horror",
+  10402: "Music",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Sci-Fi",
+  10770: "TV Movie",
+  53: "Thriller",
+  10752: "War",
+  37: "Western",
+};
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(API_URL);
-    const json = await response.json();
+    const json = await (await fetch(API_URL)).json();
     setMovies(json);
     setLoading(false);
   };
-
   useEffect(() => {
     getMovies();
   }, []);
-
   console.log(movies);
-
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
               id={movie.id}
+              year={
+                movie.release_date ? movie.release_date.split("-")[0] : "N/A"
+              }
+              coverImg={movie.poster_path}
               title={movie.title}
-              poster_path={movie.poster_path}
-              overview={movie.overview}
-              genre_ids={movie.genre_ids}
+              summary={movie.overview}
+              genres={
+                movie.genre_ids
+                  ? movie.genre_ids.map((id) => GENRE_MAP[id])
+                  : []
+              }
             />
           ))}
         </div>
@@ -40,5 +68,4 @@ function Home() {
     </div>
   );
 }
-
 export default Home;
